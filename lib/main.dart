2222f8804c75629c35d0c3c166aa56bb4742bdea
Vishnu-o5 <128472439+Vishnu-o5@users.noreply.github.com>
2024-07-28 14:50:27 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:attendence/pages/student_login.dart';
+import './widgets/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-      home: MyApp(),
+    home: MyApp(),
   ));
 }
 
@@ -16,10 +16,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[200],
-        body: SafeArea(
-          child: Center(
-            child: Column(
+      backgroundColor: Colors.grey[200],
+      body: SafeArea(
+        child: Center(
+          child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children:<Widget>[
                 Text(
@@ -30,105 +30,111 @@ class MyApp extends StatelessWidget {
                 ),
                 GestureDetector(
                   onTap: (){
-                    Navigator.push(context,MaterialPageRoute(builder: (context)=>StudentLogin()));
+                    Navigator.push(context,MaterialPageRoute(builder: (context)=>LoginPageLayout(path: 'assets/images/student_login.png', role: 'Student''s Login')));
                   },
                   child: Card(
-                    child: Stack(
-                      children: [
-                        Container(
-                          width: 225,
-                          height: 225,
-                          decoration: BoxDecoration(
-                            color: Colors.grey[300],
-                            borderRadius: BorderRadius.circular(30),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black,
-                                spreadRadius: 0.3,
-                                blurRadius: 20
-                              )
-                            ]
+                      child: Stack(
+                        children: [
+                          Container(
+                            width: 225,
+                            height: 225,
+                            decoration: BoxDecoration(
+                                color: Colors.grey[300],
+                                borderRadius: BorderRadius.circular(30),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.black,
+                                      spreadRadius: 0.3,
+                                      blurRadius: 20
+                                  )
+                                ]
+                            ),
                           ),
-                        ),
-                        Positioned.fill(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.only(top: 15),
-                                child: ClipRRect(
+                          Positioned.fill(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(top: 15),
+                                  child: ClipRRect(
                                     borderRadius: BorderRadius.circular(100),
                                     child: Image(
-                                      image: AssetImage('assets/images/student_logo.jpg'),
+                                      image: AssetImage('assets/images/student_login.png'),
                                       width: 125,
                                       height: 125,
                                       fit: BoxFit.cover,
                                     ),
                                   ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(top: 20),
-                                child: Text("Student",
-                                style: TextStyle(fontSize: 30),
                                 ),
-                              )
-                            ],
-                          ),
-                        )
-                      ],
-                    )
+                                Padding(
+                                  padding: EdgeInsets.only(top: 20),
+                                  child: Text("Student",
+                                    style: TextStyle(fontSize: 30),
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      )
                   ),
                 ),
-                Card(
-                    child: Stack(
-                      children: [
-                        Container(
-                          width: 225,
-                          height: 225,
-                          decoration: BoxDecoration(
-                              color: Colors.grey[300],
-                              borderRadius: BorderRadius.circular(30),
-                              boxShadow: [
-                                BoxShadow(
-                                    color: Colors.black,
-                                    spreadRadius: 0.3,
-                                    blurRadius: 20
-                                )
-                              ]
+
+                GestureDetector(
+                  onTap: (){
+                    Navigator.push(context,MaterialPageRoute(builder: (context)=>LoginPageLayout(path: 'assets/images/faculty_login.png', role: 'Faculty''s Login')));
+                  },
+                  child:Card(
+                      child: Stack(
+                        children: [
+                          Container(
+                            width: 225,
+                            height: 225,
+                            decoration: BoxDecoration(
+                                color: Colors.grey[300],
+                                borderRadius: BorderRadius.circular(30),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.black,
+                                      spreadRadius: 0.3,
+                                      blurRadius: 20
+                                  )
+                                ]
+                            ),
                           ),
-                        ),
-                        Positioned.fill(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.only(top: 20),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(100),
-                                  child: Image(
-                                    image: AssetImage('assets/images/teacher_icon.png'),
-                                    width: 125,
-                                    height: 125,
-                                    fit: BoxFit.cover,
+                          Positioned.fill(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(top: 20),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(100),
+                                    child: Image(
+                                      image: AssetImage('assets/images/faculty_login.png'),
+                                      width: 125,
+                                      height: 125,
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(top: 20),
-                                child: Text("Teacher",
-                                  style: TextStyle(fontSize: 30),
-                                ),
-                              )
-                            ],
-                          ),
-                        )
-                      ],
-                    )
-                )
-            ]
-            ),
+                                Padding(
+                                  padding: EdgeInsets.only(top: 20),
+                                  child: Text("Teacher",
+                                    style: TextStyle(fontSize: 30),
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      )
+                  )
+                ),
+              ]
           ),
         ),
-      );
+      ),
+    );
   }
 }
